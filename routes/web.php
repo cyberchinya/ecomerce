8ae97f6113763ccd41681e0c69ab3a37a11d2fcc
agnerfist agner <chinyanana@gmail.com>
2021-07-01 13:26:35 +0400

    migrate

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
-
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 
 /*Route::get('/', function () {
     return view('welcome');
 });*/
 
 Route::get('/', HomeComponent::class);
+
+Route::get('shop',ShopComponent::class);
+
+Route::get('/cart', CartComponent::class);
+
+Route::get('/checkout', CheckoutComponent::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
